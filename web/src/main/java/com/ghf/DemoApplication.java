e3e34444f2436c09c009e8e88884f70c4ffe7ef9
ghf704694314 <704694314@qq.com>
2017-11-28 17:57:20 +0800

    添加eureka-client

--- web/src/main/java/com/ghf/DemoApplication.java
+++ web/src/main/java/com/ghf/DemoApplication.java
@@ -14,6 +14,7 @@ import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 @EnableAutoConfiguration
 @ComponentScan
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class DemoApplication {
 
 	public static void main(String[] args) {
